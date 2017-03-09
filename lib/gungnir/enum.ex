04400fd8bf940cd327894fac2baaaaacfdb6cc89
kenta-s <knt01222@gmail.com>
2017-03-09 22:04:50 +0900

    Add docs

--- lib/gungnir/enum.ex
+++ lib/gungnir/enum.ex
@@ -1,4 +1,17 @@
 defmodule Gungnir.Enum do
+  @moduledoc """
+  Gungnir.Enum
+  """
+
+  @doc """
+  Remove nil from the list
+
+  ## Examples
+
+      iex> Gungnir.Enum.compact([1,2,nil,4])
+      [1,2,4]
+
+  """
   def compact(list) do
     Enum.filter(list, fn(x) -> x != nil end)
   end
--- lib/gungnir/string.ex
+++ lib/gungnir/string.ex
@@ -1,8 +1,37 @@
 defmodule Gungnir.String do
+  @moduledoc """
+  Gungnir.String
+  """
+
+  @doc """
+  Returns true if the string is blank.
+
+  ## Examples
+
+      iex> Gungnir.String.is_blank("")
+      true
+
+      iex> Gungnir.String.is_blank(" \n")
+      true
+
+      iex> Gungnir.String.is_blank("hello")
+      false
+  """
   def is_blank(string) do
     String.trim(string) == ""
   end
 
+  @doc """
+  Returns true if the string is present.
+
+  ## Examples
+
+      iex> Gungnir.String.is_present("hello")
+      true
+
+      iex> Gungnir.String.is_present("")
+      false
+  """
   def is_present(string) do
     String.trim(string) != ""
   end
